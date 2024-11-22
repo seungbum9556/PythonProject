24a931733cc2ec50f0d4d2790f716c11073cf2d5
ITSC <istc@example.com>
2024-11-22 20:18:00 +0900

    1

---
+++ section03/Ex03-2-print.py
@@ -0,0 +1,14 @@
+'''
+파일명 : Ex03-2-print.py
+print() 함수
+    - sep: 값 사이 구분자 (기본: 공백)
+    - end: 출력 끝 문자 (기본: \n)
+    - file: 출력 대상(기본: sys.stdout)
+
+'''
+# 1. 기본 출력
+pokemon = '피카츄'
+level = 25
+print('포켓몬:', pokemon, '레벨:', level) # ,는 자동 공백 추가
+
+# 2. sep 매개변수
