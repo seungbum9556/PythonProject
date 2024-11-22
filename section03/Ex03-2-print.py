0b28862b86c041d15d9f8a36e13d6c18c3e9a145
ITSC <istc@example.com>
2024-11-22 21:27:56 +0900

    1

--- section03/Ex03-2-print.py
+++ section03/Ex03-2-print.py
@@ -12,3 +12,21 @@ level = 25
 print('포켓몬:', pokemon, '레벨:', level) # ,는 자동 공백 추가
 
 # 2. sep 매개변수
+stats = ['피카츄', '전기', '35', '55']
+'''
+주석 단축키 : Ctrl + /
+'''
+# * -> 언패킹(unpacking) 피카츄 전기 35 55
+print(*stats, sep =',') # 쉼표로 구분
+print(*stats)
+print(stats)
+
+# 3. end 매개 변수
+print('피카츄', end='>')
+print('라이츄', end='>')
+print('파이리')
+
+# 4. 파일 출력
+with open ('pokemon.txt', 'w', encoding='UTF-8') as file:
+    print('name: 피카츄', file = file)
+    print('type: 전기', file = file)
