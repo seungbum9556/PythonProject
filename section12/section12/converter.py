7923a02fd9c4a5c0cd732763bdec512efd0ffcad
seungbum9556 <tmdqja9556@naver.com>
2024-12-05 20:31:06 +0900

    Add files via upload

---
+++ section12/section12/converter.py
@@ -0,0 +1,12 @@
+'''
+파일명: converter.py
+'''
+
+MILES = 0.621371
+POUND = 0.00330462
+
+def kilometer_to_miles(kilometer):
+    return kilometer * MILES
+
+def gram_to_pound(gram):
+    return gram * POUND
